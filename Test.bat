ba64dc339d90c3e89a90a65271bf46f0abb37215
kyivdamir <kyivdamir@gmail.com>
2025-03-08 09:51:26 +0100

    Add files via upload

---
+++ Test.bat
@@ -0,0 +1,11 @@
+@echo off
+color 2
+echo if you want to alive you need to pass the questions
+pause
+echo but firstly you need to install Python in my situation you will install x64
+pause
+open https://www.python.org/ftp/python/3.13.2/python-3.13.2-amd64.exe
+pause
+echo on you install python download this think
+pause
+open
